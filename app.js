4f6ee2a3b46009a30a8d2184c1ef5bee4398c840
levworkshop <levworkshop@users.noreply.github.com>
2022-06-09 13:13:11 +0300

    sperate function

--- app.js
+++ app.js
@@ -9,28 +9,20 @@ function addTax(price) {
 }
 
 function generateId() {
-    // const rand = Math.random();
-    // console.log(`rand: ${rand}`);
-    // const num = rand * 100000;
-    // console.log(`num: ${num}`);
-    // const floor = Math.floor(num);
-    // console.log(`floor: ${floor}`);
-    // return floor;
-
     const time = new Date().getTime();
     return Math.floor(Math.random() * time);
 }
 
 const products = [
     {
-        id: 1,
+        id: generateId(),
         price: 15,
         name: 'yellow cheese',
         image: '2016/03/05/19/24/cheese-1238395_960_720.jpg',
         description: 'great product really!',
     },
     {
-        id: 2,
+        id: generateId(),
         price: 3,
         name: 'ice cream',
         image: '2017/04/18/15/10/strawberry-ice-cream-2239377_640.jpg',
@@ -39,9 +31,8 @@ const products = [
 ];
 
 
-function displayProduct() {
+function getProductsTemplate() {
     const IMG_PATH = 'https://cdn.pixabay.com/photo/'; // configuration of the project
-    const container = document.getElementById('products-container');
     let html = '';
 
     products.forEach(prod => {
@@ -51,13 +42,20 @@ function displayProduct() {
                     class="card-img-top" alt="${prod.description}">
                 <div class="card-body">
                     <h5 class="card-title">${prod.name}</h5>
-                    <p class="card-text">
-                        ${prod.price}</p>
+                    <p class="card-text">$
+                        ${addTax(prod.price)}</p>
                     <a href="#" class="btn btn-primary">View Details</a>
                 </div>
             </div>`;
     })
 
+    return html;
+}
+
+
+function displayProduct() {
+    const container = document.getElementById('products-container');
+    const html = getProductsTemplate();
     container.innerHTML = html;
 }
 
