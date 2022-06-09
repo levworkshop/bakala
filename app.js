d4183b88e386f6780e0e6e1701fa54c9efcb2876
levworkshop <levworkshop@users.noreply.github.com>
2022-06-09 11:08:36 +0300

    display product

--- app.js
+++ app.js
@@ -20,3 +20,35 @@ function generateId() {
     const time = new Date().getTime();
     return Math.floor(Math.random() * time);
 }
+
+const products = [
+    {
+        id: 1,
+        price: 15,
+        name: 'yellow cheese',
+        image: 'https://cdn.pixabay.com/photo/2016/03/05/19/24/cheese-1238395_960_720.jpg',
+        description: 'great product really!',
+    },
+    {
+        id: 2,
+        price: 3,
+        name: 'ice cream',
+        image: 'https://cdn.pixabay.com/photo/2017/04/18/15/10/strawberry-ice-cream-2239377_640.jpg',
+        description: 'be careful - very cold',
+    },
+];
+
+function displayProduct() {
+    const container = document.getElementById('products-container');
+
+    container.innerHTML = `<div class="card" style="width: 18rem;">
+    <img src="https://cdn.pixabay.com/photo/2016/03/05/19/24/cheese-1238395_960_720.jpg"
+        class="card-img-top" alt="description of the product">
+    <div class="card-body">
+        <h5 class="card-title">Yellow Cheese</h5>
+        <p class="card-text">
+            $15.00</p>
+        <a href="#" class="btn btn-primary">View Details</a>
+    </div>
+</div>`;
+}
