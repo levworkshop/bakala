b08e0534a27e77b523279576b8256e4c4cf09051
levworkshop <noreply@githubusers.com>
2022-06-16 14:25:00 +0300

    search

--- app.js
+++ app.js
@@ -17,14 +17,14 @@ const products = [
     {
         id: 1,
         price: 15,
-        name: 'yellow cheese',
+        name: 'Yellow cheese',
         image: '2016/03/05/19/24/cheese-1238395_960_720.jpg',
         description: 'great product really!',
     },
     {
         id: 2,
         price: 3,
-        name: 'ice cream',
+        name: 'Ice cream',
         image: '2017/04/18/15/10/strawberry-ice-cream-2239377_640.jpg',
         description: 'be careful - very cold',
     },
@@ -38,6 +38,22 @@ const products = [
 ];
 
 
+function searchProduct() {
+    const searchField = document.getElementById('search-field');
+    const searchTerm = searchField.value.trim(); // ' tab '.trim() => 'tab'
+
+    const searchResult = products.filter(prod => {
+        return prod.name
+            .toLowerCase()
+            .includes(searchTerm.toLowerCase())
+    });
+
+    // console.log(searchResult); // = []
+    console.log('2) after search - call to function with search result');
+    displayProducts(searchResult);
+}
+
+
 
 
 
@@ -48,12 +64,14 @@ function getTotalProducts() {
 
 
 // take products data and add it into the html of each card
-function displayProducts() {
+function displayProducts(productData) {
+    console.log(`inside function) productData: ${productData}`);
+
     const IMG_PATH = 'https://cdn.pixabay.com/photo/'; // configuration of the project
     const container = document.getElementById('products-container');
     let html = '';
 
-    products.forEach(prod => {
+    productData.forEach(prod => {
         console.log(`${IMG_PATH}${prod.image}`);
 
         html +=
@@ -73,4 +91,5 @@ function displayProducts() {
     getTotalProducts();
 }
 
-displayProducts();
+console.log(`1) on page load - call the function with ALL the products`);
+displayProducts(products);
