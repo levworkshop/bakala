e9a341d7b5296ae8f84df9ef61afa174b40027dc
levworkshop <noreply@githubusers.com>
2022-06-21 15:21:24 +0300

    searchProduct

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+import { products } from './data-modules/products.js';
+
+
+// todo: use this function
 function addTax(price) {
     if (!price || price <= 0 || typeof (price) === 'string') {
         throw Error('Nothing is for free! please supply a price');
@@ -8,65 +12,23 @@ function addTax(price) {
     return total;
 }
 
-class Product {
-    constructor(productPrice, productName, productImage, productDescription) {
-        this.price = productPrice;
-        this.name = productName;
-        this.image = productImage;
-        this.description = productDescription;
-        this.id = this.generateId();
-    }
+/*
+window.searchProduct = function() {
+    const searchField = document.getElementById('search-field');
+    const searchTerm = searchField.value.trim(); // ' tab '.trim() => 'tab'
 
-    // for inner use - inside the class
-    generateId() {
-        const time = new Date().getTime();
-        return Math.floor(Math.random() * time);
-    }
+    const searchResult = products.filter(prod => {
+        return prod.name
+            .toLowerCase()
+            .includes(searchTerm.toLowerCase())
+    });
 
-    // for outer user - outside the class
-    getPrice() {
-        return `$${this.price}`;
-    }
-
-    getName() {
-        return this.name;
-    }
-
-    getImage() {
-        const IMG_PATH = 'https://cdn.pixabay.com/photo/'; // configuration of the project
-        return `${IMG_PATH}${this.image}`;
-    }
-
-    getDescription() {
-        return this.description;
-    }
-
-    getId() {
-        return this.id;
-    }
+    displayProducts(searchResult);
 }
 
-const products = [
-    new Product(
-        15,
-        'Yellow cheese',
-        '2016/03/05/19/24/cheese-1238395_960_720.jpg',
-        'great product really!'
-    ),
-    new Product(
-        3,
-        'Ice cream',
-        '2017/04/18/15/10/strawberry-ice-cream-2239377_640.jpg',
-        'be careful - very cold'
-    ),
-    new Product(
-        55,
-        'aaa',
-        '2017/04/18/15/10/strawberry-ice-cream-2239377_640.jpg',
-        '',
-    ),
-];
-
+// and then the button in the html has to have "onclick"
+<button id="search-btn" type="button" onclick="searchProduct()">Search</button>
+*/
 
 function searchProduct() {
     const searchField = document.getElementById('search-field');
@@ -80,6 +42,9 @@ function searchProduct() {
 
     displayProducts(searchResult);
 }
+
+const searchButton = document.getElementById(`search-btn`);
+searchButton.addEventListener('click', searchProduct);
 
 
 function getTotalProducts() {
