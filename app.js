1dd5839a7ea9d97ca73e78e4414dc148d54f9d95
levworkshop <noreply@githubusers.com>
2022-06-21 14:12:36 +0300

    class

--- app.js
+++ app.js
@@ -8,13 +8,6 @@ function addTax(price) {
     return total;
 }
 
-// function generateId() {
-//     const time = new Date().getTime();
-//     return Math.floor(Math.random() * time);
-// }
-
-
-
 class Product {
     constructor(productPrice, productName, productImage, productDescription) {
         this.price = productPrice;
@@ -53,41 +46,25 @@ class Product {
     }
 }
 
-const prod1 = new Product(
-    15,
-    'Yellow cheese',
-    '2016/03/05/19/24/cheese-1238395_960_720.jpg',
-    'great product really!'
-);
-console.log(prod1.getPrice());
-console.log(prod1.getName());
-console.log(prod1.getImage());
-console.log(prod1.getDescription());
-console.log(prod1.getId());
-
-
 const products = [
-    {
-        id: 1,
-        price: 15,
-        name: 'Yellow cheese',
-        image: '2016/03/05/19/24/cheese-1238395_960_720.jpg',
-        description: 'great product really!',
-    },
-    {
-        id: 2,
-        price: 3,
-        name: 'Ice cream',
-        image: '2017/04/18/15/10/strawberry-ice-cream-2239377_640.jpg',
-        description: 'be careful - very cold',
-    },
-    {
-        id: 3,
-        price: 55,
-        name: 'aaa',
-        image: '2017/04/18/15/10/strawberry-ice-cream-2239377_640.jpg',
-        description: '',
-    },
+    new Product(
+        15,
+        'Yellow cheese',
+        '2016/03/05/19/24/cheese-1238395_960_720.jpg',
+        'great product really!'
+    ),
+    new Product(
+        3,
+        'Ice cream',
+        '2017/04/18/15/10/strawberry-ice-cream-2239377_640.jpg',
+        'be careful - very cold'
+    ),
+    new Product(
+        55,
+        'aaa',
+        '2017/04/18/15/10/strawberry-ice-cream-2239377_640.jpg',
+        '',
+    ),
 ];
 
 
@@ -113,19 +90,19 @@ function getTotalProducts() {
 
 // take products data and add it into the html of each card
 function displayProducts(productData) {
-    const IMG_PATH = 'https://cdn.pixabay.com/photo/'; // configuration of the project
     const container = document.getElementById('products-container');
     let html = '';
 
     productData.forEach(prod => {
         html +=
             `<div class="card" style="width: 18rem;">
-                <img src="${IMG_PATH}${prod.image}"
-                    class="card-img-top" alt="${prod.description}">
+                <img src="${prod.getImage()}"
+                    class="card-img-top" alt="${prod.getDescription()}">
                 <div class="card-body">
-                    <h5 class="card-title">${prod.name}</h5>
+                    <h5 class="card-title">${prod.getName()}</h5>
                     <p class="card-text">
-                        ${addTax(prod.price)}</p>
+                        ${prod.getPrice()}
+                    </p>
                     <a href="#" class="btn btn-primary">View Details</a>
                 </div>
             </div>`;
