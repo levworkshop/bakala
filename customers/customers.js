b68f44c0e56aacdbbf289a110ac3a71709d85a78
levworkshop <noreply@githubusers.com>
2022-06-28 15:24:50 +0300

    customer by id

--- customers/customers.js
+++ customers/customers.js
@@ -30,3 +30,30 @@ function getAllCustomers() {
 }
 document.getElementById(`get-all-customers-button`)
     .addEventListener('click', getAllCustomers);
+
+
+// 2) Get customer by id
+// function getCustomerById() {
+//     const id = document.getElementById(`get-customer-id-field`).value;
+
+//     fetch(`${serverUrl}/get-customer.json?${id}`)
+
+//         .then(res => res.json())
+//         .then(customers => {
+//             let html = '';
+//             customers.forEach(singleCustomer => {
+//                 html += `<tr>
+//                     <td>${singleCustomer.id}</td>
+//                     <td>${singleCustomer.name}</td>
+//                     <td>${singleCustomer.email}</td>
+//                 </tr>`;
+//             });
+//             document.getElementById(`get-customer-table`).innerHTML = html;
+//         })
+
+//         .catch(error => {
+//             throw Error(error);
+//         })
+// }
+// document.getElementById(`get-customer-button`)
+//     .addEventListener('click', getCustomerById);
