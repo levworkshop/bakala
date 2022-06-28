7b15ed27b5a5fdbe4883ed337769a45728c94511
levworkshop <noreply@githubusers.com>
2022-06-28 14:13:29 +0300

    get all customers

---
+++ customers/customers.js
@@ -0,0 +1,27 @@
+const serverUrl = `http://127.0.0.1:3000/customers/response`;
+
+// 1) Get all customers
+function getAllCustomers() {
+    fetch(`${serverUrl}/get-all-customers.json`)
+        // `http://127.0.0.1:3000/customers/response/get-all-customers.json`
+
+        .then(res => res.json())
+
+        .then(customers => {
+            let html = '';
+            customers.forEach(singleCustomer => {
+                html += `<tr>
+                    <td>${singleCustomer.id}</td>
+                    <td>${singleCustomer.name}</td>
+                    <td>${singleCustomer.email}</td>
+                </tr>`;
+            });
+            document.getElementById(`customers-table`).innerHTML = html;
+        })
+
+        .catch(error => {
+            throw Error(error);
+        })
+}
+document.getElementById(`get-all-customers-button`)
+    .addEventListener('click', getAllCustomers);
